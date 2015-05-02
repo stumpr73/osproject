34715e5e3fa388612eb9e5a77e48f1ee087de0e6
DaveMcFave <davidferrara@live.com>
2015-05-02 12:41:41 -0400

    Update TreeTest.java
    Here is the most recent test class for the Tree class.

--- src/MemoryManagement/TreeTest.java
+++ src/MemoryManagement/TreeTest.java
@@ -5,15 +5,18 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.util.ArrayList;
+
 /**
  * The test class TreeTest.
  *
  * @author  David Ferrara
- * @version 3.31.2015
+ * @version 5.2.2015
  */
 public class TreeTest
 {
     private Tree t1;
+    private ArrayList<Tree> leaves;
     /**
      * Default constructor for test class TreeTest
      */
@@ -29,7 +32,8 @@ public class TreeTest
     @Before
     public void setUp()
     {
-        t1 = new Tree(64, null);
+        leaves = new ArrayList<Tree>();
+        t1 = new Tree(64, null, 0, leaves);
     }
 
     /**
@@ -43,36 +47,6 @@ public class TreeTest
     }
 
     @Test
-    public void testGetLeft()
-    {
-        assertEquals(null, t1.getLeft());
-    }
-
-    @Test
-    public void testSetLeft()
-    {
-        Tree t2 = new Tree(32, t1);
-        assertEquals(null, t1.getLeft());
-        t1.setLeft(t2);
-        assertEquals(t2, t1.getLeft());
-    }
-    
-    @Test
-    public void testGetRight()
-    {
-        assertEquals(null, t1.getRight());
-    }
-
-    @Test
-    public void testSetRight()
-    {
-        Tree t2 = new Tree(32, t1);
-        assertEquals(null, t1.getRight());
-        t1.setRight(t2);
-        assertEquals(t2, t1.getRight());
-    }
-    
-    @Test
     public void testGetSize()
     {
         assertEquals(64, t1.getSize());
@@ -85,7 +59,37 @@ public class TreeTest
         t1.setSize(32);
         assertEquals(32, t1.getSize());
     }
-    
+
+    @Test
+    public void testGetLeft()
+    {
+        assertEquals(null, t1.getLeft());
+    }
+
+    @Test
+    public void testSetLeft()
+    {
+        Tree t2 = new Tree(32, t1, 0, leaves);
+        assertEquals(null, t1.getLeft());
+        t1.setLeft(t2);
+        assertEquals(t2, t1.getLeft());
+    }
+
+    @Test
+    public void testGetRight()
+    {
+        assertEquals(null, t1.getRight());
+    }
+
+    @Test
+    public void testSetRight()
+    {
+        Tree t2 = new Tree(32, t1, 0, leaves);
+        assertEquals(null, t1.getRight());
+        t1.setRight(t2);
+        assertEquals(t2, t1.getRight());
+    }
+
     @Test
     public void testGetLp()
     {
@@ -100,81 +104,147 @@ public class TreeTest
         t1.setLp(p1);
         assertEquals(p1, t1.getLp());
     }
-    
+
+    @Test
+    public void testGetLowEnd()
+    {
+        assertEquals(0, t1.getLowEnd());
+    }
+
+    @Test
+    public void testSetLowEnd()
+    {
+        assertEquals(0, t1.getLowEnd());
+        t1.setLowEnd(32);
+        assertEquals(32, t1.getLowEnd());
+    }
+
     @Test
     public void testGetSizeLeft()
     {
         assertEquals(64, t1.getSizeLeft());
     }
-    
+
     @Test
-    public void testFind()
+    public void testSetSizeLeft()
     {
-        //Set up the tree.
-        Process p1, p2;
-        p1 = new Process(32, "Process A");
-        p2 = new Process(16, "Process B");
-        Tree t2, t3, t4, t5;
-        t2 = new Tree(32, t1);
-        t3 = new Tree(32, t1);
-        t4 = new Tree(16, t3);
-        t5 = new Tree(16, t3);
-        t1.setLeft(t2);
-        t1.setRight(t3);
-        t3.setLeft(t4);
-        t3.setRight(t5);
-        t2.setLp(p1);
-        t4.setLp(p2);
-        
-        //Run tests.
-        assertEquals(p1, t2.getLp());
-        assertEquals(p2, t4.getLp());
-        assertEquals(t2, t1.find(p1));
-        assertEquals(t4, t1.find(p2));
+        assertEquals(64, t1.getSizeLeft());
+        t1.setSizeLeft(32);
+        assertEquals(32, t1.getSizeLeft());
     }
-    
+
+    @Test
+    public void testGetParent()
+    {
+        assertEquals(null, t1.getParent());
+    }
+
+    @Test
+    public void testSetParent()
+    {
+        Tree t2 = new Tree(32, null, 0, leaves);
+        assertEquals(null, t2.getParent());
+        t2.setParent(t1);
+        assertEquals(t1, t2.getParent());
+    }
+
+    @Test
+    public void testHasParent()
+    {
+        Tree t2 = new Tree(32, null, 0, leaves);
+        assertEquals(false, t2.hasParent());
+        t2.setParent(t1);
+        assertEquals(true, t2.hasParent());
+    }
+
     @Test
     public void testGenerateChildren()
     {
         assertEquals(null, t1.getLeft());
         assertEquals(null, t1.getRight());
         t1.generateChildren();
-        Tree t2 = t1.getLeft();
-        Tree t3 = t1.getRight();
-        assertEquals(t2, t1.getLeft());
-        assertEquals(t3, t1.getRight());
+        assertNotNull(t1.getLeft());
+        assertNotNull(t1.getRight());
         assertEquals(32, t1.getLeft().getSize());
         assertEquals(32, t1.getRight().getSize());
+        assertEquals(t1, t1.getLeft().getParent());
+        assertEquals(t1, t1.getRight().getParent());
+        assertEquals(0, t1.getLeft().getLowEnd());
+        assertEquals(32, t1.getRight().getLowEnd());
     }
-    
+
     @Test
     public void testResetChildren()
     {
-        testGenerateChildren();
+        assertEquals(null, t1.getLeft());
+        assertEquals(null, t1.getRight());
+        t1.generateChildren();
+        assertNotNull(t1.getLeft());
+        assertNotNull(t1.getRight());
+        t1.resetChildren();
         assertEquals(null, t1.getLeft());
         assertEquals(null, t1.getRight());
     }
     
     @Test
+    public void testAreNoChildren()
+    {
+        assertEquals(null, t1.getLeft());
+        assertEquals(null, t1.getRight());
+        assertEquals(true, t1.areNoChildren());
+        t1.generateChildren();
+        assertNotNull(t1.getLeft());
+        assertNotNull(t1.getRight());
+        assertEquals(false, t1.areNoChildren());
+    }
+    
+    @Test
     public void testFindSuitableChild()
     {
-        //Set up the tree.
-        Process p1, p2;
-        p1 = new Process(32, "Process A");
-        p2 = new Process(16, "Process B");
-        Tree t2, t3, t4, t5;
-        t2 = new Tree(32, t1);
-        t3 = new Tree(32, t1);
-        t4 = new Tree(16, t3);
-        t5 = new Tree(16, t3);
-        t1.setLeft(t2);
-        t1.setRight(t3);
-        t3.setLeft(t4);
-        t3.setRight(t5);
-        t2.setLp(p1);
+        Process p1 = new Process(32, "Process A");
+        Process p2 = new Process(16, "Process B");
+        Process p3 = new Process(16, "Process C");
+        Process p4 = new Process(8, "Process D");
+        t1.generateChildren();
+        t1.getRight().generateChildren();
+    }
+    
+    @Test
+    public void testAssignProcess()
+    {
         
-        //Run Tests.
-        assertEquals(t4, t1.findSuitableChild(p2));
+    }
+    
+    @Test
+    public void testUpdateParentSize()
+    {
+        
+    }
+
+    @Test
+    public void testAllocate()
+    {
+        Process p1 = new Process(32, "Process A");
+        Process p2 = new Process(16, "Process B");
+        t1.allocate(p1);
+        assertNotNull(t1.getLeft());
+        assertEquals(p1, t1.getLeft().getLp());
+        t1.allocate(p2);
+        assertNotNull(t1.getRight().getLeft());
+        assertEquals(p2, t1.getRight().getLeft().getLp());
+    }
+
+    @Test
+    public void testDeallocate()
+    {
+        Process p1 = new Process(32, "Process A");
+        Process p2 = new Process(16, "Process B");
+        t1.allocate(p1);
+        t1.allocate(p2);
+        t1.deallocate();
+        assertNull(t1.getLeft().getLp());
+        t1.deallocate();
+        assertNull(t1.getLeft());
+        assertNull(t1.getRight());
     }
 }
-
